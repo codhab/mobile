24774ff0f21ff2d75cfb91024aba47849ecbf4ea
Elton Silva <elton.chrls@gmail.com>
2017-03-15 19:27:10 -0300

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'candidate/home#show'
 
+  resources :notifications, only: [:index]
+  
   mount Attendance::Engine => '/attendance', as: :attendance
   mount Candidate::Engine  => '/candidate',  as: :candidate
   
