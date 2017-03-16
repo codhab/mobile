75ea989bc6dde2f91e2ff01f28ecbc007b4cfed8
Elton Silva <elton.chrls@gmail.com>
2017-03-15 21:04:13 -0300

    ok

---
+++ engines/candidate/app/controllers/candidate/cadastres_controller.rb
@@ -0,0 +1,19 @@
+module Candidate
+  class CadastresController < ApplicationController
+    
+    def show
+    end
+    
+    def detail
+    end
+
+    def dependent
+    end
+
+    def pontuation
+    end
+    
+
+
+  end
+end
--- engines/candidate/config/routes.rb
+++ engines/candidate/config/routes.rb
@@ -2,6 +2,18 @@ Candidate::Engine.routes.draw do
 
   root 'home#show'
 
-  resources :cadastres, only: [:show]
+  resources :cadastres, only: [:show] do
+    collection do  
+      get 'detail',     to: 'cadastres#detail'
+      get 'pontuation', to: 'cadastres#pontuation'
+      get 'dependent',  to: 'cadastres#dependent'
+      get 'occurrence', to: 'cadastres#occurrence'
+      get 'entity',     to: 'cadastres#entity'
+      get 'situation',  to: 'cadastres#situation'
+      get 'address',    to: 'cadastres#address'
+      get 'schedule',   to: 'cadastres#schedule'
+      get 'document',   to: 'cadastres#document'
+    end
+  end
   
 end
