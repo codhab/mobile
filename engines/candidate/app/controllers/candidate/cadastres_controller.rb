c6cba2293d4567aa1dcb9ced42c76979ff0b1b80
igormarjes <igormarjes@gmail.com>
2017-03-23 12:24:09 -0300

    Update view cadastre, dependents and pontuations

--- engines/candidate/app/controllers/candidate/cadastres_controller.rb
+++ engines/candidate/app/controllers/candidate/cadastres_controller.rb
@@ -1,18 +1,22 @@
 module Candidate
   class CadastresController < ApplicationController
-    
+
     def show
     end
-    
+
     def detail
+      @cadastre = current_cadastre
     end
 
     def dependent
+      @dependents = current_cadastre.dependents.where('kinship_id <> 6')
+      @spouse = current_cadastre.dependents.where(kinship_id: 6).first
     end
 
     def pontuation
+      @pontuations = current_cadastre.pontuations
     end
-    
+
 
 
   end
