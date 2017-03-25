b6c668e9aae6db396cd84274d0731efe00629841
Elton Silva <elton.chrls@gmail.com>
2017-03-25 15:56:21 -0300

    Recadastre rules

--- engines/attendance/app/controllers/attendance/dependents_controller.rb
+++ engines/attendance/app/controllers/attendance/dependents_controller.rb
@@ -6,28 +6,62 @@ module Attendance
     before_action :set_ticket
     before_action :set_cadastre_mirror
     before_action :set_action
+    before_action :set_dependent_mirror, only: [:edit, :update, :show]
 
     def index
-      @cadatre_mirror = Core::Attendance::CadastreForm.find(@cadastre_mirror.id)
+      @cadastre_mirror = Core::Attendance::CadastreForm.find(@cadastre_mirror.id)
+    end
+
+    def show
+      @dependent_mirror = Core::Attendance::DependentForm.find(@dependent_mirror.id)
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+      @dependent_mirror = Core::Attendance::DependentForm.find(@dependent_mirror.id)
+    end
+
+    def update
+      @dependent_mirror = Core::Attendance::DependentForm.find(@dependent_mirror.id)
+      
+      if @dependent_mirror.update(set_params)
+        redirect_to ticket_continue_income_path(@ticket, @action)
+      else
+        render action: :edit
+      end
     end
 
     private
+
+    def set_params
+      params.require(:attendance_dependent_form)
+            .permit(:rg, :name, :cpf, :rg_org, :income, :born, :kinship_id, :special_condition_id)
+    end
+
+    def set_dependent_mirror
+      @dependent_mirror = @cadastre_mirror.dependent_mirrors.find(params[:id])
+    end
+
+    def set_ticket
+      @ticket = @cadastre.tickets.find(params[:ticket_id])
+      @ticket = Core::Attendance::TicketPolicy.new(@ticket)
+    end
+
+    def set_action
+      @action = @ticket.actions.find_by(context_id: 2)
+    end
 
     def set_cadastre
       @cadastre = current_cadastre
     end
 
-    def set_ticket
-      @ticket = @cadastre.tickets.find(params[:ticket_id])
-    end
-
-    def set_action
-      @action = @ticket.actions.find_by(context_id: 1)
-    end
-
     def set_cadastre_mirror
-      @cadastre_mirror = current_cadastre.cadastre_mirrors.find(@ticket.cadastre_mirror_id)
+      @cadastre_mirror = @cadastre.cadastre_mirrors.find(@ticket.cadastre_mirror_id)
     end
-
   end
 end
--- engines/attendance/app/controllers/attendance/documents_controller.rb
+++ engines/attendance/app/controllers/attendance/documents_controller.rb
@@ -8,15 +8,45 @@ module Attendance
     before_action :set_action 
 
     def new      
-      @action = Core::Attendance::ActionDocumentForm.find(@action.id)
+      @action   = Core::Attendance::ActionDocumentForm.find(@action.id)
+      
+      @service  = Core::Attendance::DocumentService.new(cadastre: @cadastre,
+                                                        action: @action,
+                                                        ticket: @ticket) 
+      
+      @service.documents_required!
+
+      @action = @service.action
     end
 
     def create
+      if @action.update(set_params)
+        @service = Core::Attendance::TicketService.new(cadastre: @cadastre, ticket: @ticket, action: @action)
+        @service.close_action
+
+        redirect_to new_ticket_path
+      else
+        render action: :new
+      end
+
     end
 
     private
 
     def set_params
+      params.fetch(:attendance_action_document_form, {})
+            .permit(
+              certificate_born_documents: [:id, :_destroy, :document],
+              income_documents_attributes: [:id, :_destroy, :document],
+              rg_documents: [:id, :_destroy, :document],
+              cpf_documents: [:id, :_destroy, :document],
+              residence_documents: [:id, :_destroy, :document],
+              arrival_df_documents: [:id, :_destroy, :document],
+              registry_documents: [:id, :_destroy, :document],
+              payment_documents: [:id, :_destroy, :document],
+              income_documents: [:id, :_destroy, :document],
+              special_condition_documents: [:id, :_destroy, :document],
+              )
     end
 
     def set_cadastre
