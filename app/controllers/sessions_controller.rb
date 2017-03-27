1bb139f7c64e5a1b59fe37de9fe289c94ffa6e7d
Elton Silva <elton.chrls@gmail.com>
2017-03-27 16:24:05 -0300

    Removes deprecated method text/plain

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -36,7 +36,5 @@ class SessionsController < ActionController::Base
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
     headers['Access-Control-Max-Age'] = '1728000'
-
-    render :text => '', :content_type => 'text/plain'
   end
 end
