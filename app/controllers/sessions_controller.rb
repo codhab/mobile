2737ec7fc2fc356dbf1d161e7157867b8faf2602
Elton Silva <elton.chrls@gmail.com>
2017-03-27 16:17:09 -0300

    Fix cors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -32,13 +32,11 @@ class SessionsController < ActionController::Base
   end
 
   def cors_preflight_check
-    if request.method == 'OPTIONS'
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-      headers['Access-Control-Max-Age'] = '1728000'
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+    headers['Access-Control-Max-Age'] = '1728000'
 
-      render :text => '', :content_type => 'text/plain'
-    end
+    render :text => '', :content_type => 'text/plain'
   end
 end
