1ea211a69a4baf9193b29e37579494fa3ad9e07d
Victor Vitalino <vitalino.victor@gmail.com>
2017-05-04 15:23:13 -0300

    protocol dependency

--- engines/protocol/app/controllers/protocol/application_controller.rb
+++ engines/protocol/app/controllers/protocol/application_controller.rb
@@ -1,3 +1,6 @@
+require_dependency 'application_controller'
+require_dependency 'application_helper'
+
 module Protocol
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
