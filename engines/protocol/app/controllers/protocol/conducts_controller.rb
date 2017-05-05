0eda76eb9865ee98f9f4983ab47aabec0ea1658f
igormarjes <igormarjes@gmail.com>
2017-05-05 17:42:44 -0300

    Update token

--- engines/protocol/app/controllers/protocol/conducts_controller.rb
+++ engines/protocol/app/controllers/protocol/conducts_controller.rb
@@ -3,6 +3,9 @@ module Protocol
   class ConductsController < ApplicationController # :nodoc:
     def index
       @staff = Core::Person::Staff.find(session[:staff_id])
+      if params[:deviceid].present? && params[:pushtoken].present?
+        @staff.update(mobile_user_token: params[:deviceid], mobile_push_token: params[:pushtoken])
+      end
       @due_documents = Core::View::DueDocument.where('responded is not true and sector_destiny_id = ?', @staff.sector_current_id) if @staff.present?
     end
   end
