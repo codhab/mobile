4a321e8ad588c8951b84b3ddd664c7705dff881e
Elton Silva <elton.chrls@gmail.com>
2017-12-27 16:30:14 -0200

    Aluguel legal

--- engines/candidate/app/controllers/candidate/legal_rents_controller.rb
+++ engines/candidate/app/controllers/candidate/legal_rents_controller.rb
@@ -4,6 +4,23 @@ module Candidate
   class LegalRentsController < ApplicationController
 
     def new
+
+      if !Candidate::LegalRent.where(cadastre_id: current_cadastre.id).present?
+        
+        if params[:status] == 'true'
+          @rent = Candidate::LegalRent.new(cadastre_id: current_cadastre.id)
+          @rent.status = true
+          @rent.save
+        end
+
+        if params[:status] == 'false'
+          @rent = Candidate::LegalRent.new(cadastre_id: current_cadastre.id)
+          @rent.status = false
+          @rent.save
+        end
+
+      end
+
     end
 
   end
