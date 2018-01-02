717aa6772663de80ec2dd24fb69c361c17ba8450
Elton Silva <elton.chrls@gmail.com>
2018-01-02 15:44:36 -0200

    Ok

--- engines/candidate/app/controllers/candidate/legal_rents_controller.rb
+++ engines/candidate/app/controllers/candidate/legal_rents_controller.rb
@@ -6,11 +6,20 @@ module Candidate
     def new
 
       if !Candidate::LegalRent.where(cadastre_id: current_cadastre.id).present?
-        
+
         if params[:status] == 'true'
-          @rent = Candidate::LegalRent.new(cadastre_id: current_cadastre.id)
-          @rent.status = true
-          @rent.save
+
+          respond_to do |format|
+            format.js {
+              
+            }
+
+            format.html {
+              @rent = Candidate::LegalRent.new(cadastre_id: current_cadastre.id)
+              @rent.status = true
+              @rent.save
+            }
+          end
         end
 
         if params[:status] == 'false'
