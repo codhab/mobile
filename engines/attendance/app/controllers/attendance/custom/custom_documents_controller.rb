687f0914f99f1b5e6ae0a46d32c2b1a751eeffbb
Elton Silva <elton.chrls@gmail.com>
2018-02-23 20:37:07 -0300

    ok

--- engines/attendance/app/controllers/attendance/custom/custom_documents_controller.rb
+++ engines/attendance/app/controllers/attendance/custom/custom_documents_controller.rb
@@ -18,17 +18,12 @@ module Attendance
 
         @document.save
 
-        if params[:type].to_s == 'document'
-          @ticket.documents.where(document_type_id: 5).count >= 7
-          @ticket.update(action_five: true)
-        end
-
         redirect_to new_custom_custom_ticket_custom_document_path(dependent_mirror_id: params[:dependent_mirror_id], type: params[:type], anchor: params[:anchor])
       end
 
       def destroy
         @ticket.documents.find(params[:id]).destroy
-        
+
         redirect_to new_custom_custom_ticket_custom_document_path(dependent_mirror_id: params[:dependent_mirror_id], type: params[:type], anchor: params[:anchor])
       end
 
