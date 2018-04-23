194be241d7ea32fef99afcaa4c9a4b1518313266
Victor Vitalino <vitalino.victor@gmail.com>
2018-04-23 14:28:59 -0300

    fix paths

--- engines/regularization/config/routes.rb
+++ engines/regularization/config/routes.rb
@@ -4,10 +4,11 @@ Regularization::Engine.routes.draw do
     resources :solicitation_documents,  as: :documents
   end
 
-  get 'error',  to: 'solicitations#error',            as: 'error' 
-  get 'search', to: 'solicitation_monitoring#index',  as: 'monitoring'
-  get 'block',  to: 'solicitations#block',            as: 'block'
-  get 'group',  to: 'solicitations#group',            as: 'group'
-  get 'unit',   to: 'solicitations#unit',             as: 'unit'
+  get 'error',     to: 'solicitations#error',            as: 'error' 
+  get 'complete',  to: 'solicitations#complete',         as: 'complete' 
+  get 'search',    to: 'solicitation_monitoring#index',  as: 'monitoring'
+  get 'block',     to: 'solicitations#block',            as: 'block'
+  get 'group',     to: 'solicitations#group',            as: 'group'
+  get 'unit',      to: 'solicitations#unit',             as: 'unit'
   # get 'by_cpf',    to: 'solicitation_monitoring#index'
 end
