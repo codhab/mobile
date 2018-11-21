e560086063d0bc6afe8759d0b053331a55e25462
Victor Vitalino <vitalino.victor@gmail.com>
2018-11-21 15:38:45 -0200

    fix test

--- engines/technical_assistance/app/models/technical_assistance/pre_register.rb
+++ engines/technical_assistance/app/models/technical_assistance/pre_register.rb
@@ -1,6 +1,6 @@
 module TechnicalAssistance
   class PreRegister < ApplicationRecord
-    self.table_name = 'extranet.technical_assistance_pre_registers'
+    self.table_name = 'generic.social_work_candidate_schedules'
     belongs_to :city,          required: false, class_name: 'Core::Address::City'
     validates :address, :name, presence: true
     validates :cpf, cpf: true, presence: true
