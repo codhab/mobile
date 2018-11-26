b03eddf0eaa65be99044e378237d39abe1c93e6c
Igor Marques <igormarjes@gmail.com>
2018-11-26 09:33:45 -0200

    Link entity

--- engines/entity/app/controllers/entity/indications_controller.rb
+++ engines/entity/app/controllers/entity/indications_controller.rb
@@ -12,7 +12,7 @@ module Entity
       if params[:enterprise_id].present?
         session[:enterprise_id] = params[:enterprise_id]
       end
-      
+
       @enterprise = current_entity.enterprises.find(session[:enterprise_id])
       @indication = Core::Entity::Indication.new(enterprise_id: @enterprise.id)
     end
@@ -22,7 +22,7 @@ module Entity
       @indication = Core::Entity::Indication.new(set_params)
 
       @indication.enterprise_id = @enterprise.id
-      
+
       if @indication.valid?
         @indication.persist_indication
 
@@ -37,14 +37,14 @@ module Entity
       if @unit.update(situation: 'candidato_indicado')
         @unit.indicate_candidate!
       end
-      
+
       redirect_to new_indication_path(indication_id: @unit.id)
     end
 
     def cancel
       @unit = @units.find(params[:indication_id])
       @unit.update(cadastre_id: nil, situation: 'indicado_para_entidade')
-      
+
       @unit.indication_logs.create({
         cadastre_id: @unit.cadastre_id,
         description: "Candidato removido."
@@ -68,6 +68,5 @@ module Entity
     def set_units
       @units = Core::Entity::IndicationUnit.where(entity_id: current_entity.id)
     end
-
   end
 end
