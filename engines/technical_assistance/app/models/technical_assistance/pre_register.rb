a3dc4e1ed7946e2db472b54fe621c07cbb2c5893
Igor Marques <igormarjes@gmail.com>
2018-12-04 09:00:15 -0200

    Atualizar

--- engines/technical_assistance/app/models/technical_assistance/pre_register.rb
+++ engines/technical_assistance/app/models/technical_assistance/pre_register.rb
@@ -4,5 +4,12 @@ module TechnicalAssistance
     belongs_to :city,          required: false, class_name: 'Core::Address::City'
     validates :address, :name, presence: true
     validates :cpf, cpf: true, presence: true
+
+    before_save :set_order
+
+    def set_order
+      new_order = TechnicalAssistance::PreRegister.where(city_id: self.city_id).order(created_at: :asc).last.order
+      self.order = new_order.present? ? new_order + 1 : 1
+    end
   end
 end
