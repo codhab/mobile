43f176161fa69ea0399a1809d8878b6c54ba3292
Elton Silva <elton.chrls@gmail.com>
2020-03-09 14:31:42 -0300

    OK g

--- engines/candidate/app/controllers/candidate/enterprises_controller.rb
+++ engines/candidate/app/controllers/candidate/enterprises_controller.rb
@@ -2,20 +2,21 @@ module Candidate
   class EnterprisesController < ApplicationController
     
     def new
-      enterprise = Candidate::Enterprise.new
+      enterprise = ::Candidate::Enterprise.new
       
       enterprise.cadastre_id = current_cadastre.id
-      enterprise.acept_id = params[:acept_id]
 
-        if enterprise.save
-          if enterprise.acept_id == "1"
-            redirect_to success_path
-          else
-            redirect_to root_path
-          end
-        else
-          redirect_to root_path
-        end
+      if params[:accept] == 'sim' 
+        enterprise.accepted = true 
+        enterprise.save
+        
+        redirect_to success_path
+      else
+        enterprise.accepted = false 
+        enterprise.save
+        
+        redirect_to root_path
+      end
     end
 
   end
--- engines/candidate/app/models/candidate/enterprise.rb
+++ engines/candidate/app/models/candidate/enterprise.rb
@@ -2,42 +2,38 @@ module Candidate
   class Enterprise
     include ActiveModel::Model 
 
-    attr_accessor :cadastre_id, :acept_id
+    attr_accessor :cadastre_id, :accepted
 
     validates :cadastre_id, presence: true
 
     def save
       if valid?
-        if acept_id == "1"
+        if accepted
           enterprise_cadastre = Core::Candidate::EnterpriseCadastre.new(
-
             cadastre_id: self.cadastre_id,
-            enterprise_id: 22,
-            indication_type_id: 1,
-            indication_situation_id: 1,
-            manifestation_date: Time.now,
+            enterprise_id: 182,
+            indication_type_id: 999,
+            manifestation_date: Time.current,
             accepted: true,
-            accepted_date: Time.now,
+            accepted_date: Time.current,
             inactive: false
-
           )
-          enterprise_cadastre.save
+          enterprise_cadastre.save(validate: false)
+
         else
           enterprise_cadastre = Core::Candidate::EnterpriseCadastre.new(
-
             cadastre_id: self.cadastre_id,
-            enterprise_id: 22,
-            indication_type_id: 55,
-            manifestation_date: Time.now,
+            enterprise_id: 182,
+            indication_type_id: 999,
+            manifestation_date: Time.current,
             accepted: false,
-            accepted_date: Time.now,
+            accepted_date: Time.current,
             inactive: true,
-            inactive_at: Time.now
-
+            inactive_at: Time.current
           )
-        
-          enterprise_cadastre.save
+          enterprise_cadastre.save(validate: false)
         end
+
       else
         false
       end
